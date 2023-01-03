24471471e8570749972bb7a88a506f1202f9c516
dMonkng <97328697+dMonkng@users.noreply.github.com>
2023-01-03 18:32:31 +0530

    final

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Script i satt i en egen fil");
+console.log("Script i satt i en egen fil")
+document.querySelector('.cross').getElementsByClassName.display = "none";
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if (document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display = "inline";
+        document.querySelector('.cross').style.display = "none";
+    }
+    else{
+        document.querySelector('.ham').style.display = "none";
+        setTimeout(()=>{
+            document.querySelector('.cross').style.display = "inline";
+        },350);
+    }
+})
